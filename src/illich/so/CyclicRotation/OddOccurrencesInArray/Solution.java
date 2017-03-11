9efba7955e9a5f4c87bc898a014bb4cc3f0e5df2
illichso <illichso@gmail.com>
2017-03-11 22:45:10 +0100

    add task

--- src/illich/so/CyclicRotation/OddOccurrencesInArray/Solution.java
+++ src/illich/so/CyclicRotation/OddOccurrencesInArray/Solution.java
@@ -1,8 +1,25 @@
 package illich.so.CyclicRotation.OddOccurrencesInArray;
 
 class Solution {
+//   For example, given array A = [1,2,3,4,5] and K = 0, the function should return [1,2,3,4,5].
+//   For example, given array A = [1,2,3,4,5] and K = 1, the function should return [5,1,2,3,4].
+//   For example, given array A = [1,2,3,4,5] and K = 2, the function should return [4,5,1,2,3].
+//   For example, given array A = [1,2,3,4,5] and K = 3, the function should return [3,4,5,1,2].
+//   For example, given array A = [1,2,3,4,5] and K = 4, the function should return [2,3,4,5,1].
+//   For example, given array A = [1,2,3,4,5] and K = 5, the function should return [1,2,3,4,5].
 
-    int getSolution(int[] A) {
-        return 0;
+
+//   For example, given array A = [3, 8, 9, 7, 6] and K = 2, the function should return [6, 3, 8, 9, 7].
+//   For example, given array A = [3, 8, 9, 7, 6] and K = 2, the function should return [7, 6, 3, 8, 9].
+//   For example, given array A = [3, 8, 9, 7, 6] and K = 3, the function should return [9, 7, 6, 3, 8].
+
+    int[] getSolution(int[] A, int K) {
+        int result[] = new int[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+            int rotatedIndex = i + K < A.length ? i + K : (i + K) % A.length;
+            result[rotatedIndex] = A[i];
+        }
+        return result;
     }
 }
