b2948cd03111ff3ed6231c25df3e7836629da27f
illichso <illichso@gmail.com>
2017-03-11 11:07:14 +0100

    add XOR implementstion

--- src/illich/so/OddOccurrencesInArray/Main.java
+++ src/illich/so/OddOccurrencesInArray/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         int A [] = {9, 3, 9, 3, 9, 7, 9};
-        System.out.println(solution.solution(A));
+        System.out.println(solution.getSolution(A));
 
     }
 
--- src/illich/so/OddOccurrencesInArray/Solution.java
+++ src/illich/so/OddOccurrencesInArray/Solution.java
@@ -8,7 +8,16 @@ import static java.util.stream.Collectors.toList;
 
 class Solution {
 
-    int solution(int[] N) {
+    int getSolution(int[] N) {
+        int result = 0;
+
+        for (int element : N) {
+            result = result ^ element; // Implementation of XOR
+        }
+        return result;
+    }
+
+    int getFunctionalSolution(int[] N) {
         List<Integer> integerList = stream(N).boxed().collect(toList());
         return integerList.stream()
                 .filter(e -> frequency(integerList, e) == 1)
