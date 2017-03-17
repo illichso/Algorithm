f5f9588c3c21d20e17cb15b617d246047e0fe32c
illichso <illichso@gmail.com>
2017-03-17 16:15:21 +0100

    update

--- src/main/java/illich/so/MissingInteger/Main.java
+++ src/main/java/illich/so/MissingInteger/Main.java
@@ -1,31 +1,46 @@
 package illich.so.MissingInteger;
 
+
+
 public class Main {
 
     private static Solution solution = new Solution();
 
     public static void main(String[] args) {
-        int X1 = 5;
-        int A1[] = {1, 3, 1, 4, 2, 3, 5, 4};
+        int A1[] = {4, 1, 3, 2};
+        int A2[] = {4, 1, 3};
+        int A3[] = {1};
+        int A4[] = {3};
+        int A5[] = {1, 2};
+        int A6[] = {2, 1};
+        int A7[] = {1, 3};
+        int A8[] = {3, 1};
+        int A9[] = {1, 1, 3, 4};
 
-        calculateResult(X1, A1);
-
+        calculateResult(A1);
+        calculateResult(A2);
+        calculateResult(A3);
+        calculateResult(A4);
+        calculateResult(A5);
+        calculateResult(A6);
+        calculateResult(A7);
+        calculateResult(A8);
+        calculateResult(A9);
     }
 
-    private static void calculateResult(int X, int[] A) {
-        long result = solution.getSolution(X, A);
-        System.out.print("For X: " + X);
-        System.out.println(" and A:");
+    private static void calculateResult(int[] A) {
+        long result = solution.getSolution(A);
+        System.out.println("Array:");
 
         for (int element : A) {
             System.out.print(element);
             System.out.print(";");
         }
         System.out.println();
-        if (result > 0) {
-            System.out.println("the earliest time when the frog can jump to the other side of the river: " + result + ".");
-        } else if (result == -1) {
-            System.out.println("the frog is never able to jump to the other side of the river,");
+        if (result == 1) {
+            System.out.println("is a permutation.");
+        } else if (result == 0) {
+            System.out.println("is NOT a permutation.");
         }
         System.out.println("=================================");
     }
--- src/main/java/illich/so/MissingInteger/Solution.java
+++ src/main/java/illich/so/MissingInteger/Solution.java
@@ -1,30 +1,37 @@
 package illich.so.MissingInteger;
 
-//For example, given X = 5 and array A such that:
+//For example, given:
 //
 //        A[0] = 1
 //        A[1] = 3
-//        A[2] = 1
+//        A[2] = 6
 //        A[3] = 4
-//        A[4] = 2
-//        A[5] = 3
-//        A[6] = 5
-//        A[7] = 4
-//        the function should return 6, as explained above.
+//        A[4] = 1
+//        A[5] = 2
+//        the function should return 5.
 
 import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
 
-    int getSolution(int X, int[] A) {
+    int getSolution(int A[]) {
+        int theoreticalElement = 0;
+        long realSum = 0;
+        long theoreticalSum = 0;
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < A.length; i++) {
-            set.add(A[i]);
-            if (set.size() == X) {
-                return i;
-            }
+
+        for (int realElement : A) {
+            set.add(realElement);
+
+            theoreticalElement++;
+
+            realSum += realElement;
+            theoreticalSum += theoreticalElement;
         }
-        return -1;
+        if(set.size() == A.length && theoreticalSum - realSum == 0){
+            return 1;
+        }
+        return 0;
     }
 }
