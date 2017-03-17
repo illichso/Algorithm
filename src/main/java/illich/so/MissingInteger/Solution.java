3e036781c2433a86a66f4dc72fb1def6e82b060c
illichso <illichso@gmail.com>
2017-03-17 16:12:47 +0100

    add task

---
+++ src/main/java/illich/so/MissingInteger/Solution.java
@@ -0,0 +1,30 @@
+package illich.so.MissingInteger;
+
+//For example, given X = 5 and array A such that:
+//
+//        A[0] = 1
+//        A[1] = 3
+//        A[2] = 1
+//        A[3] = 4
+//        A[4] = 2
+//        A[5] = 3
+//        A[6] = 5
+//        A[7] = 4
+//        the function should return 6, as explained above.
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    int getSolution(int X, int[] A) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            set.add(A[i]);
+            if (set.size() == X) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
