867e34b4b8de1c6a6cf2711bfe4f7ff8a73c3530
illichso <illichso@gmail.com>
2017-03-19 09:57:12 +0100

    add

--- src/main/java/illich/so/MaxCounters/Solution.java
+++ src/main/java/illich/so/MaxCounters/Solution.java
@@ -1,19 +1,26 @@
 package illich.so.MaxCounters;
 
-//For example, given X = 5 and array A such that:
-//
-//        A[0] = 1
-//        A[1] = 3
-//        A[2] = 1
-//        A[3] = 4
-//        A[4] = 2
-//        A[5] = 3
-//        A[6] = 5
-//        A[7] = 4
-//        the function should return 6, as explained above.
-
 import java.util.HashSet;
 import java.util.Set;
+
+//For example, given integer N = 5 and array A such that:
+//        A[0] = 3
+//        A[1] = 4
+//        A[2] = 4
+//        A[3] = 6
+//        A[4] = 1
+//        A[5] = 4
+//        A[6] = 4
+//        the values of the counters after each consecutive operation will be:
+//
+//        (0, 0, 1, 0, 0)
+//        (0, 0, 1, 1, 0)
+//        (0, 0, 1, 2, 0)
+//        (2, 2, 2, 2, 2)
+//        (3, 2, 2, 2, 2)
+//        (3, 2, 2, 3, 2)
+//        (3, 2, 2, 4, 2)
+//        The goal is to calculate the value of every counter after all operations.
 
 class Solution {
 
